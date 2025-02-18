8d2d3eda0c622ac5b8f723f9be2f14698b6541c1
david molina <molinasalasdavid08@gmail.com>
2025-02-18 12:52:49 -0300

    fix5

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/ecommerce-react">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
