c8f567bc68268ee4d814580550873d9741de480d
david molina <molinasalasdavid08@gmail.com>
2025-02-18 12:00:36 -0300

    fix4

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ecommerce-react/",
+  base: "/ecommerce-react/", // Reemplaza con el nombre de tu repositorio en GitHub
+  build: {
+    outDir: "dist", // Asegura que la salida se haga en la carpeta dist
+  },
 });
